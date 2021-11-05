b36019d16fdbed86c0d3febae0609e74730cd0f9
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-05 14:49:07 -0300

    Primeiros estudos Javascript

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/aula-01-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,14 @@
+let myName = "Diego venturin";
+let birthCity = "Concórdia";
+let birthYear = 1987;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Lindóia";
+console.log(birthCity);
