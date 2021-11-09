0e0ae7fdf19ec5ed15fff3afc569f8f1a5f813a0
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-09 07:33:49 -0300

    Programas com operações matemáticas

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/aula-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/aula-01-javascript-primeiros-passos/scripts.js
@@ -1,7 +1,31 @@
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+//Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. 
 
+let a = 1;
+let b = 2;
 
+let resultado;
 
-for (let index = 0; index < groceryList.length; index += 1) {
-    console.log(groceryList[index]);
-}
+//Adição 
+
+resultado = a + b;
+console.log(resultado);
+
+//Subtração 
+
+resultado = a - b;
+console.log(resultado);
+
+//Multiplicação 
+
+resultado = a * b;
+console.log(resultado);
+
+//Divisão 
+
+resultado = a / b;
+console.log(resultado);
+
+//Divisão 
+
+resultado = a % b;
+console.log(resultado);
