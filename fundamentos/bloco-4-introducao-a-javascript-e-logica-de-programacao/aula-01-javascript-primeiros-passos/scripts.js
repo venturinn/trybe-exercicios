4e9639528c2a9dbb91084e3417441892a8d45100
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-09 09:21:31 -0300

    Exercício 03 adicionado

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/aula-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/aula-01-javascript-primeiros-passos/scripts.js
@@ -32,8 +32,10 @@ console.log(resultado);
 
 //Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
 
-let c = 11;
-let d = 12;
+const c = 11;
+const d = 11;
+const e = 11;
+
 
 if (c > d) {
     console.log('C= ' + c);
@@ -41,4 +43,23 @@ if (c > d) {
     console.log('D= ' + d);
 } else {
     console.log('Números iguais!');
+}
+
+//Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+
+
+if (c >= d && c > e) {
+    console.log(c);
+} else if (c > d && c >= e) {
+    console.log(c);
+} else if (d >= c && d > e) {
+    console.log(d);
+} else if (d > c && d >= e) {
+    console.log(d);
+} else if (e >= c && e > d) {
+    console.log(e);
+} else if (e > c && e >= d) {
+    console.log(e);
+} else {
+    console.log('Todos iguais!');
 }
