cad5484909bed3778dbde6a9a176af1d95aaba8d
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-09 07:46:26 -0300

    Exercício número 02 adiconado

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/aula-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/aula-01-javascript-primeiros-passos/scripts.js
@@ -29,3 +29,16 @@ console.log(resultado);
 
 resultado = a % b;
 console.log(resultado);
+
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+let c = 11;
+let d = 12;
+
+if (c > d) {
+    console.log('C= ' + c);
+} else if (c < d) {
+    console.log('D= ' + d);
+} else {
+    console.log('Números iguais!');
+}
