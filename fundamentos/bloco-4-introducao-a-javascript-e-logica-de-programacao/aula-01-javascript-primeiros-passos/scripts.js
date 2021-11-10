02b4eb98ccd403cf5d8c92be9cdff125aa90283d
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-09 22:30:58 -0300

    Adicionados os exerícios 10 e 11

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/aula-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/aula-01-javascript-primeiros-passos/scripts.js
@@ -168,3 +168,103 @@ if (num01 % 2 != 0 || num02 % 2 != 0 || num03 % 2 != 0) {
 } else {
     console.log(false);
 }
+
+/*Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+Atente que, sobre o custo do produto, incide um imposto de 20%.
+Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+valorCustoTotal = valorCusto + impostoSobreOCusto
+lucro = valorVenda - valorCustoTotal (lucro de um produto)*/
+
+const valorCompra = 50;
+const valorVenda = 61;
+
+let custo = (valorCompra * 1.2);
+
+if (valorCompra < 0 || valorVenda < 0) {
+    console.log('Valores Negativos');
+} else {
+    console.log((1000 * valorVenda) - (1000 * custo));
+}
+
+/*Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
+A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
+INSS (Instituto Nacional do Seguro Social)
+Salário bruto até R$ 1.556,94: alíquota de 8%
+Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
+Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
+Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
+IR (Imposto de Renda)
+Até R$ 1.903,98: isento de imposto de renda
+De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
+De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
+De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
+Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
+Exemplo : Uma pessoa possui o salário bruto de R$ 3.000,00. O cálculo será:
+O salário bruto está entre R$ 2.594,93 e R$ 5.189,82, então sua alíquota para INSS é de 11%. O INSS será 11% de R$ 3.000, ou seja, R$ 330,00.
+Para descobrir o salário-base, subtraia do salário bruto a alíquota do INSS: R$ 3.000,00 - R$ 330,00 = R$ 2.670,00.
+Para pegar o valor do IR, temos um salário (já deduzido o INSS) entre R$ 1.903,99 e 2.826,65, sendo a alíquota, então, de 7.5%, com parcela de R$ 142,80 a deduzir do imposto. Assim, temos:
+R$ 2.670,00: salário com INSS já deduzido;
+7.5%: alíquota de imposto de renda;
+R$ 142,80 parcela a se deduzir do imposto.
+Fazendo a conta, temos: (7,5% de R$ 2.670,00) - R$ 142,80 = R$ 57,45
+O último cálculo para conseguir o salário líquido é R$ 2.670,00 - R$ 57,45 (salário-base - valor IR) = R$ 2.612,55.
+Resultado: R$ 2.612,55.
+Dica: que tal identificar as alíquotas com variáveis de nomes explicativos?*/
+
+let salary = 3000;
+
+let inss8 = 0.08;
+let inss9 = 0.09;
+let inss11 = 0.11;
+
+let salaryInss8 = 0;
+let salaryInss9 = 0;
+let salaryInss11 = 0;
+let salaryInssMax = 0;
+
+
+// Descobrir alíquota INSS:
+
+if (salary <= 1556.95 && salary > 0) {
+    salaryInss8 = salary - (salary * inss8);
+} else if (salary >= 1556.95 && salary <= 2594.92) {
+    salaryInss9 = salary - (salary * inss9);
+} else if (salary >= 2594.93 && salary <= 5189.82) {
+    salaryInss11 = salary - (salary * inss11);
+} else if (salary > 5189.82) {
+    salaryInssMax = salary - 570.88;
+} else {
+    console.log('Você não paga para trabalhar!')
+}
+
+let salaryInss;
+
+if (salaryInss8 != 0) {
+    salaryInss = salaryInss8;
+} else if (salaryInss9 != 0) {
+    salaryInss = salaryInss9;
+} else if (salaryInss11 != 0) {
+    salaryInss = salaryInss11;
+} else if (salaryInssMax != 0) {
+    salaryInss = salaryInssMax;
+} else {
+    console.log('O cálculo de desconto de INSS não foi realizado')
+}
+
+
+// Descobrir alíquota IR:
+
+if (salaryInss < 1903.98 && salaryInss > 0) {
+    console.log('IR = 0% || Salário Líquido= ' + salaryInss);
+} else if (salaryInss >= 1903.99 && salaryInss <= 2826.65) {
+    console.log('IR = 7,5% || Salário Líquido= ' + (salaryInss - (salaryInss * 0.075) + 142.80));
+} else if (salaryInss >= 2826.66 && salaryInss <= 3751.05) {
+    console.log('IR = 15% || Salário Líquido= ' + (salaryInss - (salaryInss * 0.15) + 354.80));
+} else if (salaryInss >= 3751.06 && salaryInss <= 4664.68) {
+    console.log('IR = 22,5% || Salário Líquido= ' + (salaryInss - (salaryInss * 0.225) + 636.13));
+} else if (salaryInss > 4664.68) {
+    console.log('IR = 27,5% || Salário Líquido= ' + (salaryInss - (salaryInss * 0.275) + 869.36));
+} else {
+    console.log('Você não paga para trabalhar!')
+}
