529086bbebb12c8989abf10186479e56e7504259
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-10 10:42:40 -0300

    Adicionados os exercicios 5, 6, 7, 8 e 9 da aula 02

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,3 +1,5 @@
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
@@ -30,3 +32,58 @@ if (mediaArray > 20) {
 }
 
 // Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+
+let maiorValorArray = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+
+    if (maiorValorArray < numbers[index]) {
+        maiorValorArray = numbers[index];
+    }
+}
+console.log(maiorValorArray);
+
+// Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+let somaImparesArray = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+
+    let modulo = numbers[index] % 2;
+
+    if (modulo != 0) {
+        somaImparesArray = somaImparesArray + 1;
+    }
+}
+console.log(somaImparesArray);
+
+// Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
+let menorValorArray = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+
+    if (menorValorArray > numbers[index]) {
+        menorValorArray = numbers[index];
+    }
+}
+console.log(menorValorArray);
+
+// Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let novoArray = [];
+
+for (let index = 1; index <= 25; index += 1) {
+    novoArray.push(index);
+}
+console.log(novoArray);
+
+// Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+let divisaoNovoArray = [];
+
+for (let index = 0; index < novoArray.length; index += 1) {
+    divisaoNovoArray.push(novoArray[index] / 2);
+}
+console.log(divisaoNovoArray);
