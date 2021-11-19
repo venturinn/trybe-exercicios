7c63d258bd54785269b053debf4fcd11e3323f02
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-19 20:53:18 -0300

    Adicionado exercício 5.3 - 1

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -25,10 +25,32 @@ Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a clas
 
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
+function createDaysofMonth() {
 
+    const monthDaysList = document.querySelector('#days');
+
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+
+        const days = dezDaysList[index];
+        const dayListItem = document.createElement('li');
+        dayListItem.className = 'day';
+        dayListItem.innerHTML = days;
+
+        if (days === 24 || days === 31) {
+            dayListItem.className = 'holyday, day';
+        } else if (days === 4 || days === 11 || days === 18) {
+            dayListItem.className = 'friday, day';
+        } else if (days === 25) {
+            dayListItem.className = 'friday, day, holyday';
+        }
+
+        monthDaysList.appendChild(dayListItem);
+    }
 
 
 
 
 
 }
+
+createDaysofMonth();
