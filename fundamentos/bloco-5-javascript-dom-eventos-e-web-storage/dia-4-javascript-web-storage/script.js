14c112ef440440244c5732c84b9253d16d4662b2
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-22 12:09:49 -0300

    Adicionados os exercícios 5.4 - 4 e 5

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -1,4 +1,4 @@
-// Alterar a cor de fundo da página
+// Alterar a cor de fundo da página.
 
 function backgroudColor(event) {
 
@@ -23,7 +23,7 @@ enter.addEventListener('keypress', backgroudColor);
 let backgroudColorMemory = localStorage.getItem('backgroudColor');
 document.body.style.backgroundColor = backgroudColorMemory;
 
-// Alterar cor do texto 
+// Alterar cor do texto.
 
 function textColor(event) {
 
@@ -48,11 +48,14 @@ let enter2 = document.getElementById('text-color');
 enter2.addEventListener('keypress', textColor);
 
 let textColorMemory = localStorage.getItem('textColor');
+
+//Pega o texto para as futuras alterações:
 let mainText = document.getElementsByTagName('main');
 
 mainText[0].style.color = textColorMemory;
 
-// Alterar tamanho da fonte
+
+// Alterar tamanho da fonte.
 
 function fontSize(event) {
 
@@ -79,3 +82,60 @@ enter3.addEventListener('keypress', fontSize);
 let textSizeMemory = localStorage.getItem('textSize');
 
 mainText[0].style.fontSize = textSizeMemory + 'px';
+
+// Alterar espaçamento entre as linhas do texto.
+
+function espacamento(event) {
+
+    localStorage.setItem('espaçamento', enter4.value);
+
+    if (event.key === 'Enter' || event.type === 'click') {
+
+        if (enter4.value === '') {
+            alert('Escolha o tamanho do texto!');
+        } else {
+
+            mainText[0].style.lineHeight = enter4.value + 'px';
+
+        }
+    }
+}
+
+let add4 = document.getElementById('entrelinhas-button');
+add4.addEventListener('click', espacamento);
+
+let enter4 = document.getElementById('entrelinhas');
+enter4.addEventListener('keypress', espacamento);
+
+let espacoMemory = localStorage.getItem('espaçamento');
+
+mainText[0].style.lineHeight = espacoMemory + 'px';
+
+
+// Alterar tipo da fonte ( Font family ).
+
+function font(event) {
+
+    localStorage.setItem('fonte', enter5.value);
+
+    if (event.key === 'Enter' || event.type === 'click') {
+
+        if (enter5.value === '') {
+            alert('Escolha a fonte do texto!');
+        } else {
+
+            mainText[0].style.fontFamily = enter5.value;
+
+        }
+    }
+}
+
+let add5 = document.getElementById('font-button');
+add5.addEventListener('click', font);
+
+let enter5 = document.getElementById('font');
+enter5.addEventListener('keypress', font);
+
+let fontFamilyMemory = localStorage.getItem('fonte');
+
+mainText[0].style.fontFamily = fontFamilyMemory;
