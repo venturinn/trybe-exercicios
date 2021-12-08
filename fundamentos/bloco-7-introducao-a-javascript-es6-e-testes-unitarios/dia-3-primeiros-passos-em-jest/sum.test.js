caef0af88b96ebdf60f10f94810e2a1b82e6bff4
¨Venturin¨ <diegoventurinn@gmail.com>
2021-12-08 11:54:49 -0300

    Adicionado exercício 02.

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,16 +1,32 @@
-const sum = require ('./sum');
+const { MyRemove: myRemove, sum: sum } = require("./sum");
 
-describe('Exercício 01', () => {
-test('sum dois valores', () => {
+describe("Exercício 01", () => {
+  test("sum dois valores", () => {
     expect(sum(4, 5)).toBe(9);
   });
-  test('sum valores zero ', () => {
+  test("sum valores zero", () => {
     expect(sum(0, 0)).toBe(0);
   });
-  test('sum um valor é string', () => {
-    expect(() => { sum(4,'5') }).toThrow();
+  test("sum um valor é string", () => {
+    expect(() => {
+      sum(4, "5");
+    }).toThrow();
   });
-  test('sum mensagem de erro', () => {
-    expect(() => { sum(4,'5') }).toThrowError(new Error('parameters must be numbers'));
+  test("sum mensagem de erro", () => {
+    expect(() => {
+      sum(4, "5");
+    }).toThrowError(new Error("parameters must be numbers"));
+  });
+});
+
+describe("Exercício 02", () => {
+  test("myRemove remove item correto", () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+  test("myRemove retorna valor diferente de", () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+  test("myRemove não remove item que não existe", () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
   });
 });
