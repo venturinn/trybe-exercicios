76f6632030bafea391a2cf5430ee098c0d5263a4
¨Venturin¨ <diegoventurinn@gmail.com>
2022-01-04 11:44:45 -0300

    Adicionado exercício 04

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise4.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise4.js
@@ -64,3 +64,46 @@ const books = [
   ];
   
   // Adicione o código do exercício aqui:
+  // Encontre o livro com o maior nome.
+
+
+const expectedResult = {
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+};
+
+// Minhas solução:
+
+function longestNamedBook() {
+ 
+  let who = 0;
+  let resultado = books.reduce((acc, curr, index) => {
+    if (acc.length > curr.name.length) return acc; 
+    who = index;
+    return curr.name;
+  }, ''); 
+  
+  return books[who];
+
+}
+
+console.log(longestNamedBook());
+
+// Solução gabarito Trybe:
+
+function longestNamedBook2() {
+  return books.reduce((biggestBook, currentBook) => {
+    if (currentBook.name.length > biggestBook.name.length) {
+      return currentBook;
+    }
+    return biggestBook;
+  });
+}
+
+console.log(longestNamedBook());
