49b5a2ac23201005c468f9854148b5979e31e52d
¨Venturin¨ <diegoventurinn@gmail.com>
2022-01-04 10:49:23 -0300

    Adicionados os exercícios 01 e 02

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
@@ -0,0 +1,18 @@
+// Todos os exercícios devem ser realizados utilizando reduce , e se necessário outra HOF , a informação será citada no enunciado.
+
+// Dada uma matriz, transforme em um array.
+
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+  ];
+  
+  function flatten() {
+
+    // Solução do gabarito da Trybe, a função concat era desconhecida por mim!
+    return arrays.reduce((acc, curr) => acc.concat(curr), []);
+
+  }
+
+console.log(flatten());
