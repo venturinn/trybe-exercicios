4267f9376018ae497592751046ff139e3b911fa3
¨Venturin¨ <diegoventurinn@gmail.com>
2022-01-21 12:45:24 -0300

    Realizados os exercícios 01 e 02

--- desenvolvimento-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/primeiro-exercicio-react/src/App.js
+++ desenvolvimento-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/primeiro-exercicio-react/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const compromissos = ['Estudar HTML', 'Estudar CSS', 'Estudar JS', 'Estudar React']
+
+// Minha solução: 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    compromissos.map((compromisso)=>Task(compromisso))
   );
 }
+
+
+// Solução gabarito Trybe:
+
+/*
+class App extends React.Component {
+  render() {
+    return (
+      <ul>{ compromissos.map(tarefa => Task(tarefa)) }</ul>
+    );
+  }
+}
+*/
 
 export default App;
